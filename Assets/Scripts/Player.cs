d94028a15e945440b20e1866b4e1281b172c3ce4
Lil' Bị <trandinhduc142@gmail.com>
2025-10-20 01:52:29 +0700

    Update Indev v.0.1

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,104 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    private Rigidbody2D playerRigid;
+    private Animator playerAnimator;
+
+    public Collider2D[] colliders;
+    [Header("Attack details")]
+
+    [Header("Movement details")]
+    [SerializeField] private float moveSpeed = 3.5f;
+    [SerializeField] private float jumpForce = 8;
+    private float xInput;
+    private bool facingRight = true;
+    private int facingRightValue = 1;
+    private bool canMove = true;
+    private bool canJump = true;
+
+    [Header("Collision details")]
+    [SerializeField] private float groundCheckDistance;
+    [SerializeField] private LayerMask whatIsGround;
+    private bool isGrounded;
+
+
+    private void Awake()
+    {
+        playerRigid = GetComponent<Rigidbody2D>();
+        playerAnimator = GetComponentInChildren<Animator>();
+    }
+
+    private void Update()
+    {
+        HandleCollision();
+        HandleInput();
+        HandleMovement();
+        HandleAnimations();
+        HandleFlip();
+    }
+
+    public void EnableMovements(bool enable)
+    {
+        canMove = enable;
+        canJump = enable;
+    }
+
+    private void HandleAnimations()
+    {
+        playerAnimator.SetFloat("xVelocity", playerRigid.linearVelocity.x);
+        playerAnimator.SetFloat("yVelocity", playerRigid.linearVelocity.y);
+        playerAnimator.SetBool("IsGrounded", isGrounded);
+    }
+
+    private void HandleInput()
+    {
+        xInput = Input.GetAxisRaw("Horizontal");
+        if (Input.GetKeyDown(KeyCode.W))
+            TryToJump();
+        if (Input.GetKeyDown(KeyCode.Mouse0))
+            TryToAttack();
+    }
+    private void TryToAttack()
+    {
+        if (isGrounded)
+            playerAnimator.SetTrigger("Attack!");
+    }
+
+    private void HandleMovement()
+    {
+        if (canMove)
+            playerRigid.linearVelocity = new Vector2(xInput * moveSpeed, playerRigid.linearVelocity.y);
+        else
+            playerRigid.linearVelocity = new Vector2(0, playerRigid.linearVelocity.y);
+    }
+
+    private void TryToJump()
+    {
+        if (isGrounded && canJump)
+            playerRigid.linearVelocity = new Vector2(playerRigid.linearVelocity.x, jumpForce);
+    }
+    private void HandleFlip()
+    {
+        if (playerRigid.linearVelocity.x > 0 && facingRight == false)
+            Flip();
+        else if (playerRigid.linearVelocity.x < 0 && facingRight == true)
+            Flip();
+    }
+    private void Flip()
+    {
+        transform.Rotate(0, 180, 0);
+        facingRight = !facingRight;
+        facingRightValue *= -1;
+    }
+    private void HandleCollision()
+    {
+        isGrounded = Physics2D.Raycast(transform.position, Vector2.down, groundCheckDistance, whatIsGround);
+    }
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = Color.green;
+        Gizmos.DrawLine(transform.position, transform.position + new Vector3(0, -groundCheckDistance));
+    }
+}
--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # Mansion Mania
+
 2D Pixel RPG Platformer created with Unity Engine
+Current version: Indev v0.3
